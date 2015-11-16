c3a39151a8496073fcfc7ed17173fd0c6cbfd40d
Gianluca <gl051@biccari.com>
2015-11-15 22:45:12 -0800

    Added help command

--- download-backup.sh
+++ download-backup.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # Author Gianluca Biccari
-# Synchronize remote backup directory with a local directory, 
-# then remove files succesfully transfered.
-
-# usage: 
-#	shell:$> download_backup
 
 # Edit the following parameters according with your situation
 REMOTE_LOCATION=www.biccari.com
 BACKUP_REMOTE=${REMOTE_LOCATION}:/home/biccaric/backups/wp-sites/
 BACKUP_LOCAL=/Users/Gianluca/Backups/Site5/Wordpress
+
+# SECTION: help
+display_usage() {
+  echo "- Description:"
+	echo "Download all WordPress Sites backups from ${REMOTE_LOCATION}"
+  echo "Backups are saved into ${BACKUP_LOCAL}"
+  echo "- Usage:"
+  echo "get_wp_backup.sh"
+}
+if [ $# = 1 ] && ([ $1 = "-help" ] || [ $1 = "-h" ])
+  then
+    display_usage
+    exit 0
+fi
+# END_SECTION
 
 # Download backups
 echo "Copy backups from ${REMOTE_LOCATION} started"
